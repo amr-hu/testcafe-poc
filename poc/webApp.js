a9fe52a1c7d41153067736c6750c879e699c191f
amrhusseinsayed <44497975+amrhusseinsayed@users.noreply.github.com>
2021-12-07 19:32:28 +0200

    log url

--- poc/webApp.js
+++ poc/webApp.js
@@ -1,6 +1,11 @@
+const getURL = ClientFunction(() => window.location.href);
+
 fixture("Web App Fixture");
 
 test("My Test", async (t) => {
-    await t.navigateTo("http://192.168.1.5:8090/getName")
-        .expect(false).ok();
+    await t.navigateTo("http://192.168.1.5:8090/getName");
+
+    console.log("Page URL: " + await getURL());
+
+    await t.expect(false).ok();
 });
