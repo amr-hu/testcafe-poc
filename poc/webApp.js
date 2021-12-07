3796e5e9ee2a70d5475febef2508757133cbaf6c
amrhusseinsayed <44497975+amrhusseinsayed@users.noreply.github.com>
2021-12-07 17:53:25 +0200

    https

--- poc/webApp.js
+++ poc/webApp.js
@@ -1,6 +1,6 @@
 fixture("Web App Fixture");
 
 test("My Test", async (t) => {
-    await t.navigateTo("http://192.168.1.5:8090/getName")
+    await t.navigateTo("https://192.168.1.5:8090/getName")
         .expect(false).ok();
 });
